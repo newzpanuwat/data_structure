e3f17643afa6d20dc50a1e2fc60593e6ff601043
New Panuwat <pfcreatorit@gmail.com>
2019-07-09 00:16:03 +0700

    merge sorted part1

---
+++ data_structure/merge_sort.py
@@ -0,0 +1,59 @@
+# Binary search
+
+# Merge sort
+# Split 2 small array and smaller until got single array
+
+def merge_sort(list):
+    """
+    Sorts a list in ascending order
+    Return a new sorted list
+    Divide: Find the midpoint of the list and divide into sublists
+    Conquer: Recursively sort the sublists created in previous step
+    Combine: Merge the sorted sublists created in previous step
+    """
+
+    if len(list) <= 1:
+        return list
+
+    left_half, right_half = split(list)
+    left = merge_sort(left_half)
+    right = merge_sort(right_half)
+
+    return merge(left, right)
+
+def split(list):
+    """
+    Divide the unsorted ist at midpoint sublists
+    Return two sublists - left and right
+
+    """
+
+    mid = len(list) // 2
+    left = list[:mid]
+    right = list[mid:]
+
+    return left, right
+
+def merge(left, right):
+    """
+    Merges two lists (arrays), sorting them in the process
+    Returns a new merged list
+    """
+
+    l = []
+    i = 0
+    j = 0
+    
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            l.append(left[i])
+            i += 1
+        else:
+            l.append(right[j])
+            j += 1
+    while i < len(left):
+        l.append(left[i])
+        i += 1
+    while j < len(right):
+        l.append(right[j])
+        j += 1
