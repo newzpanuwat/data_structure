9c5d83c45a770262e095c7047744f5a0d6bb93af
New Panuwat <pfcreatorit@gmail.com>
2019-07-12 23:22:26 +0700

    intro algorithm

---
+++ data_structure/recursive_function.py
@@ -0,0 +1,35 @@
+# Recursion and space complexity
+call function -> Recursive depth
+
+2 way measuring the effiency runtime algorithm
+
+# Time complexity
+- Time complexity is runtime complexity grows and grows logic.
+
+# Space complexity
+- Space complexity is a measure if how much working storage, or extra storage, is needed as a particular
+algorithm grows.
+- Memory used
+- Worse case scenario
+
+Space complexity of old binary search = N
+
+Space complexity of Recursive binary search = N, N/2, N/4 O(log n)
+
+Recap
+
+Why we learn ?
+
+Algorithmic Thinking
+- Problem solving
+- Did everyday
+- know binary search how to search faster
+- know how to sort data which unsorted
+
+Big O
+- Time complexity, gold is how to understand evaluate algorithm
+- we learn runtime Constant, Linear, Logarithmic, Quadratic
+- Linear search, Binary search
+
+Recursion
+- Recursive binary search
